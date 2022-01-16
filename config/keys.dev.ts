1f5ab351bacddf2bed483af076f3294dc7be8a08
R. Heeffer <roy1989@live.nl>
2022-01-16 20:27:05 +0100

    set up dev mongo uri

---
+++ config/keys.dev.ts
@@ -0,0 +1,5 @@
+// the following contains development configuration
+// in a usual production environment configuration is injected on deployment by varies ways
+export default {
+  mongoDbUri: 'mongodb://127.0.0.1:27017',
+};
